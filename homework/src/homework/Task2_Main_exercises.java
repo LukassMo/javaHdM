bbcbbe88c5b12bc8d6ecf9bd0a0e7ded473408aa
Lukas M <31657456+LukassMo@users.noreply.github.com>
2022-10-16 16:29:27 +0200

    Update Task2_Main_exercises.java

--- homework/src/homework/Task2_Main_exercises.java
+++ homework/src/homework/Task2_Main_exercises.java
@@ -2,4 +2,13 @@ package homework;
 
 public class Task2_Main_exercises {
 
+	public static void main(String[] args) {
+		boolean a = true, b = false, c = true;
+		boolean d, e, f, g;
+		d = !a || b;
+		e = (d && !c) || !a;
+		f = ((d == e) || (d != e)) == true;
+		g = 5 == 7;
+	}
+
 }
