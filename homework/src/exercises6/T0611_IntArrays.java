de0ca685b7067524a939265879c8cefe6530ca4b
nintendowfi <nintendowfi11@gmail.com>
2022-11-06 17:50:09 +0100

    dxdd

--- homework/src/exercises6/T0611_IntArrays.java
+++ homework/src/exercises6/T0611_IntArrays.java
@@ -2,7 +2,7 @@ package exercises6;
 
 public class T0611_IntArrays {
 	public static void main(String[] args) {
-		SubQuestion_m();
+		SubQuestion_a(); //Enter e.g "SubQuestion_a, SubQuestion_b, SubQuestion_c..."
 	}
 
 	// My Code for Sub Question A
@@ -55,6 +55,7 @@ public class T0611_IntArrays {
 		int[] a = { 3, 6, 1, 2, 10, 32, 13, 89, 3, 2, 1, 4 };
 		int[] b = new int[a.length];
 	}
+
 	static void SubQuestion_i() {
 		int[] a = { 3, 6, 1, 2, 10, 32, 13, 89, 3, 2, 1, 4 };
 		int[] b = new int[a.length];
@@ -62,28 +63,31 @@ public class T0611_IntArrays {
 		b[5] = a[5];
 		b[8] = a[8];
 	}
+
 	static void SubQuestion_j() {
 		int[] a = { 3, 6, 1, 2, 10, 32, 13, 89, 3, 2, 1, 4 };
 		int[] b = new int[a.length];
 		b[3] = a[3];
 		b[5] = a[5];
 		b[8] = a[8];
-		System.out.println(b[0]);	//Any integer array index that is not set to a value by the programmmer is initialized to a zero value.
+		System.out.println(b[0]); // Any integer array index that is not set to a value by the programmmer is
+									// initialized to a zero value.
 	}
-	
+
 	static void SubQuestion_k() {
 		int[] a = { 3, 6, 1, 2, 10, 32, 13, 89, 3, 2, 1, 4 };
 		int[] c = new int[a.length];
 	}
-	
+
 	static void SubQuestion_l() {
 		int[] a = { 3, 6, 1, 2, 10, 32, 13, 89, 3, 2, 1, 4 };
 		int[] c = new int[a.length];
 		for (int x = 0; x < c.length; x++) {
 			c[x] = a[x];
-			System.out.println(c[x]); //print to check if c array at index x is correct.
+			System.out.println(c[x]); // print to check if c array at index x is correct.
 		}
 	}
+
 	static void SubQuestion_m() {
 		int[] a = { 3, 6, 1, 2, 10, 32, 13, 89, 3, 2, 1, 4 };
 		int[] b = new int[a.length];
@@ -94,20 +98,23 @@ public class T0611_IntArrays {
 		for (int x = 0; x < c.length; x++) {
 			c[x] = a[x];
 		}
-		
-		for (int x = 0; x < a.length; x++) {	//prints the a array
+
+		for (int x = 0; x < a.length; x++) { // prints the a array
 			System.out.print("a[" + x + "]: ");
 			System.out.print(a[x] + " ");
-		}System.out.println("");
-		for (int x = 0; x < b.length; x++) {	//prints the b array
+		}
+		System.out.println("");
+		for (int x = 0; x < b.length; x++) { // prints the b array
 			System.out.print("b[" + x + "]: ");
 			System.out.print(b[x] + " ");
-		}System.out.println("");
-		for (int x = 0; x < c.length; x++) {	//prints the c array
+		}
+		System.out.println("");
+		for (int x = 0; x < c.length; x++) { // prints the c array
 			System.out.print("c[" + x + "]: ");
 			System.out.print(c[x] + " ");
-		}System.out.println("");
-		
+		}
+		System.out.println("");
+
 	}
-	 
+
 }
